c17f7ee7accfd4492cf7f41d09a8e29ede0e81da
Silin Na <n1207n@gmail.com>
2017-01-19 15:45:38 -0800

    feat: Created container component for TodoList that takes redux state and action dispatches

---
+++ src/containers/VisibleTodoList.js
@@ -0,0 +1,53 @@
+import React, { PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+import * as actions from '../actions/todoActions';
+import * as visibilityFilters from '../constants/visibilityFilters';
+
+import TodoList from '../components/TodoList';
+
+const VisibleTodoList = (props) => {
+  const { todos, actions } = props;
+
+  return (
+    <TodoList todos={todos} onTodoClick={actions.toggleTodo}/>
+  );
+};
+
+VisibleTodoList.propTypes = {
+  todos: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    completed: PropTypes.bool.isRequired,
+    text: PropTypes.string.isRequired,
+  }).isRequired).isRequired,
+  actions: PropTypes.object.isRequired,
+};
+
+function getVisibleTodoList(state, filter) {
+  switch (filter) {
+    case visibilityFilters.SHOW_ACTIVE:
+      return state.todos.filter(todo => !todo.completed);
+    case visibilityFilters.SHOW_COMPLETED:
+      return state.todos.filter(todo => todo.completed);
+    default:
+      return state.todos;
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    todos: getVisibleTodoList(state, state.filter),
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators(actions, dispatch),
+  };
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(VisibleTodoList);
